a45b30b43ac2e32b6eee29ee91b6ed9632cc56c2
SammLauzon <samuellauzon8@gmail.com>
2021-09-30 11:00:24 -0400

    Change some code in gitignore

--- .\build/core/wiring_pulse.S.d
+++
@@ -1,2 +0,0 @@
-/Users/samuellauzon/Sam/Ecole/AUT2021/GPA788/LAB1/ProjetIOT/.\build/core/wiring_pulse.S.o: \
- /Applications/Arduino.app/Contents/Java/hardware/arduino/avr/cores/arduino/wiring_pulse.S
